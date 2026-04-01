34b01c961ed13d46bdf6a41ea8a5e23bd689895b
alber <albegree@stjohns.school.nz>
2026-04-01 14:19:01 +1300

    EXPECTED CAse data testing finished "That says boundary" -  Cooper Burns

--- InsuranceApp/Program.cs
+++ InsuranceApp/Program.cs
@@ -1,59 +1,117 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Data;
+using System.Text.RegularExpressions;
 using System.Threading.Channels;
+using System.Xml.Linq;
 
 namespace InsuranceApp
 {
     internal class Program
     {
         // Global Variables
-        public static decimal deviceCost = 0;
+        public static int laptopCounter = 0 , desktopCounter = 0 , otherCounter = 0, quanity;
+        public static double totalCost = 0;
+        public static double deviceCost;
+       
         static void Main(string[] args)
         {
-            // Local Variables
-            List<string> QUESTIONS = new List<string> { "Enter Device Category:", "Enter the Device Name:","Enter Device Cost:"};
-            List<string> DEVICECATERGORY = new List<string> { "Laptop", "Desktop", "Other"};
             // Opening page and title and app discription
             Console.WriteLine(" _____                                          ___              \r\n|_   _|                                        / _ \\             \r\n  | | _ __  ___ _   _ _ __ __ _ _ __   ___ ___/ /_\\ \\_ __  _ __  \r\n  | || '_ \\/ __| | | | '__/ _` | '_ \\ / __/ _ \\  _  | '_ \\| '_ \\ \r\n _| || | | \\__ \\ |_| | | | (_| | | | | (_|  __/ | | | |_) | |_) |\r\n \\___/_| |_|___/\\__,_|_|  \\__,_|_| |_|\\___\\___\\_| |_/ .__/| .__/ \r\n                                                    | |   | |    \r\n                                                    |_|   |_|    ");
             Console.WriteLine("\n\n\n\nThis device insurance app lets users track which devices they have insured and see the cost of each insurance plan in \none convenient place. Users can quickly view all their devices, check coverage details, and monitor insurance expenses\nwithout digging through paperwork.");
-            Console.WriteLine("\n\n\n\nPress Alt f4 to Continue");
+            Console.WriteLine("\n\n\n\nPress Enter to Continue");
             Console.ReadLine();
             Console.Clear();
 
-            // Ask for Category and Name
-            Console.WriteLine(QUESTIONS[0]);
-            Console.ReadLine();
-            Console.WriteLine(QUESTIONS[1]);
-            Console.ReadLine();
-            Console.WriteLine(QUESTIONS[2]);
-            deviceCost = Convert.ToDecimal(Console.ReadLine());
-            
+            // check y or n method
             // Check if they want to add another device
             char continueInput = 'y';
             while (continueInput == 'y' || continueInput.Equals('y'))
             {
-                Console.WriteLine();
 
-                Console.WriteLine("Press enter to continue");
+                Console.WriteLine(OneDevice());
+
+                Console.WriteLine("Press Enter to continue");
                 Console.ReadLine();
-
                 Console.Clear();
 
 
                 continueInput = CheckContinueInput("\n\nDo you want to add another device? (y/n)");
 
                 Console.Clear();
+
             }
-            
+            //check if hours worked is greater than currnt top hours worked
+
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine($"Category 1(Laptop) has  {laptopCounter} devices insured\n");
+            Console.WriteLine($"Category 2(Desktop) has {desktopCounter} devices insured\n");
+            Console.WriteLine($"Category 3(Other) has {otherCounter} devices insured\n");   
+            Console.WriteLine($"The Device Total Cost:{CalculateDiscount(quanity,deviceCost,totalCost):C}");
+            Console.WriteLine("-----------------------------------------------------------------------");
         }
-        // 6 months insurance and generate top device and cost
+        // generate top device and cost
         static string OneDevice()
         {
-            for(decimal i = )
+            // Local Variables
+            List<string> QUESTIONS = new List<string> { $"Enter Device Category:", $"Enter the Device Name:", "Enter the number of devices from the device name from before", "Enter Device Cost:" };
+            
+            string deviceName;
+            int deviceCategory;
+            
+            
+
+
+            // Ask for Category,Name,N.O of Devices and Cost
+            Console.WriteLine(QUESTIONS[0]);
+            deviceCategory = Convert.ToInt32(Console.ReadLine());
+            CheckName(QUESTIONS[1]);
+            deviceName = Console.ReadLine();
+            Console.WriteLine(QUESTIONS[2]);
+            quanity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(QUESTIONS[3]);
+            deviceCost = Convert.ToDouble(Console.ReadLine());
+
+            // calculate insurance cost/amount
+            
+            // show device summary
+            Console.Clear();
+            Console.WriteLine("------------------Device Added-------------------");
+            Console.WriteLine($"The device name is {deviceName}");
+            Console.WriteLine(CalculateDepression(deviceCost));
+            Console.WriteLine($"The Devices Category is {deviceCategory}\n");
+            Console.WriteLine($"The Devices cost is {deviceCost:C}");
+            Console.WriteLine("-------------------------------------------------");
+            // add device cost to total insurance
+
+            // If statment that adds catergory thing
+            if (deviceCategory == 1)
             {
-
+                laptopCounter += quanity;
             }
-
-            return "Device Insured";
+            else if (deviceCategory == 2)
+            {
+                desktopCounter += quanity;
+            }
+            else
+            {
+                otherCounter += quanity;
+            }
+            
+            return "";
+        }
+        // 6 months depression
+        public static string CalculateDepression(double deviceCost)   
+        {
+            //Local Variables
+            double depressionCost = deviceCost;
+            string depressionSummary = "";
+            for (int monthCounter = 1;  monthCounter < 7; monthCounter++)
+            {
+                // Calculate the device depression
+                depressionCost = depressionCost * 0.95;
+                depressionSummary += $"Month {monthCounter}: {depressionCost:C}\n";
+            }
+            
+            return depressionSummary;
         }
         // Check if y or n is y or n for another device or to not want to add
         static char CheckContinueInput(string question)
@@ -77,5 +135,72 @@ namespace InsuranceApp
 
             }
         }
+        // Calculate the above 5 device discount
+        public static double CalculateDiscount(int quanity,double deviceCost,double totalCost)
+        {
+            
+            if (quanity > 5)
+            {
+                totalCost += (5 * deviceCost) + ((quanity - 5) * 0.9);
+            }
+            else
+            {
+                totalCost += quanity * deviceCost;
+            }
+
+            return totalCost;
+        }
+        // Check if a name is lowercase and convert to capital if needed
+        static string CheckName(string question)
+        {
+            while (true)
+            {
+                //ask user for name input
+                Console.WriteLine(question);
+
+                string nameInput = Console.ReadLine();
+
+                // check if name input is alphabetical characters and '-' only
+                if (Regex.IsMatch(nameInput, @"^[A-Za-z-0-9]+$"))
+                {
+                    nameInput = nameInput[0].ToString().ToUpper() + nameInput.Substring(1);
+
+                    return nameInput;
+                }
+
+            }
+
+        }
+        //Check if cost and quantity of devices
+        static int ChechInt(string question, int min, int max)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine(question);
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+
+                    //check if user input a number between a min and max
+                    if (userInput >= min && userInput <= max)
+                    {
+                        return userInput;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Please enther a number between {min} and {max}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: You must enter a number between {min} and {max}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+            }
+        }
     }
+
 }
